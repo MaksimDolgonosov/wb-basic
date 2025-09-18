744cc0d2ceca705f1027490eb686bd8ee73575f3
MAKSIM <max_air@bk.ru>
2025-09-18 16:48:33 +0300

    add splugins

--- webpack.config.ts
+++ webpack.config.ts
@@ -20,12 +20,18 @@ export default (env: IEnvironment) => {
   const config: webpack.Configuration = {
     mode: env.mode ?? "development",
     entry: "./src/index.ts", // Точка входа для сборки проекта
+    output: {
+      filename: "[name].[contenthash:8].js", // Имя выходного файла сборки
+      path: path.resolve(__dirname, "dist"), // Путь для выходного файла сборки
+      clean: true,
+    },
     devtool: isDev ? "inline-source-map" : false,
     devServer: isDev
       ? {
           port: env.port ? env.port : 5000,
           static: "./dist",
           open: true,
+          liveReload: true,
         }
       : undefined,
     module: {
@@ -39,19 +45,21 @@ export default (env: IEnvironment) => {
           test: /\.s[ac]ss$/i,
           use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
       ],
     },
     resolve: {
       extensions: [".tsx", ".ts", ".js"],
     },
 
-    output: {
-      filename: "bundle.js", // Имя выходного файла сборки
-      path: path.resolve(__dirname, "dist"), // Путь для выходного файла сборки
-      clean: true,
-    },
     plugins: [
-      new MiniCssExtractPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "css/[name].[contenthash:8].css",
+        chunkFilename: "css/[name].[contenthash:8].css",
+      }),
       new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") }),
       isDev && new webpack.ProgressPlugin(),
     ],
