4560aacf95b6963f059ccdec8fa484df7b2da5b3
MAKSIM <max_air@bk.ru>
2025-09-19 15:44:15 +0300

    add asset/resourse

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { sum } from "./sum";
 import click from "./components/button";
+import log from "./assets/img/logo.png";
+
+const log = require("./assets/img/logo.png");
 const res = sum(5, 5);
 document.querySelector("button").addEventListener("click", click);
 console.log("Hi, sum is: " + res);
+
+document.body.innerHTML = `
+<img src="./assets/img/logo.png" alt="logo" />
+`;
