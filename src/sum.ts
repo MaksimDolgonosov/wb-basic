8cfd48dfc67ca3623d9bef2998dffeb38cc61094
MAKSIM <max_air@bk.ru>
2025-10-17 15:29:57 +0300

    prettier

--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,4 @@
 export function sum(a: number, b: number) {
+  console.log('smt');
   return a + b + 100;
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,14 @@
 // const path = require("path");
 //const HtmlWebpackPlugin = require("html-webpack-plugin");
 // const webpack = require("webpack");
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import path from "path";
-import webpack from "webpack";
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ESLintPlugin from 'eslint-webpack-plugin';
+import path from 'path';
+import webpack from 'webpack';
 
-type Mode = "production" | "development";
+type Mode = 'production' | 'development';
 
 interface IEnvironment {
   mode?: Mode;
@@ -15,54 +16,62 @@ interface IEnvironment {
 }
 
 export default (env: IEnvironment) => {
-  const isDev = env.mode === "development";
+  const isDev = env.mode === 'development';
 
   const config: webpack.Configuration = {
-    mode: env.mode ?? "development",
-    entry: "./src/index.ts", // Точка входа для сборки проекта
+    mode: env.mode ?? 'development',
+    entry: './src/index.ts', // Точка входа для сборки проекта
     output: {
-      filename: "[name].[contenthash:8].js", // Имя выходного файла сборки
-      path: path.resolve(__dirname, "dist"), // Путь для выходного файла сборки
+      filename: '[name].[contenthash:8].js', // Имя выходного файла сборки
+      path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
       clean: true,
-      assetModuleFilename: "assets/[hash][ext]",
+      assetModuleFilename: 'assets/[hash][ext]'
     },
-    devtool: isDev ? "inline-source-map" : false,
+    devtool: isDev ? 'inline-source-map' : false,
     devServer: isDev
       ? {
           port: env.port ? env.port : 5000,
-          static: "./dist",
+          static: './dist',
           open: true,
-          liveReload: true,
+          liveReload: true
         }
       : undefined,
     plugins: [
       new MiniCssExtractPlugin({
-        filename: "css/[name].[contenthash:8].css",
-        chunkFilename: "css/[name].[contenthash:8].css",
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css'
       }),
-      new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") }),
-      isDev && new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src', 'index.html')
+      }),
+      new ESLintPlugin({
+        extensions: ['ts'],
+        exclude: 'node_modules',
+        fix: true, // Автоматически исправлять ошибки
+        formatter: 'stylish'
+      }),
+      isDev && new webpack.ProgressPlugin()
     ],
     module: {
       rules: [
         {
           test: /\.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
+          use: 'ts-loader',
+          exclude: /node_modules/
         },
         {
           test: /\.s[ac]ss$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset/resource",
-        },
-      ],
+          type: 'asset/resource'
+        }
+      ]
     },
     resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
+      extensions: ['.tsx', '.ts', '.js']
+    }
   };
 
   return config;
